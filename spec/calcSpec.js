cc33ab5dcc99f4d56d3cadf54712cca3bf27b676
valentina-b <valentina.bedi@gmail.com>
2020-03-03 19:13:31 +0000

    Fix tests

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,14 +1,23 @@
-describe("Calculator", function(){
-    describe("Addition tests", function(){
-        it("should return 42", function(){
-            expect(addition(20, 22)).toBe(42);
+describe("Calculator", function() {
+
+    var calc = new Calculator;
+
+    describe("Addition function", function() {
+        it("should return 42", function() {
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
         });
-        it("should return 26", function(){
-            expect(addition(19, 7)).toBe(26);
+
+        it("should return 26", function() {
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
-        it("should return an error if we don't supply two numbers", function(){
+
+        it("should return an error if we don't supply two numbers", function() {
             spyOn(window, "alert");
-            addition("Hi", "Bye");
+            calc.add("Hitchhikers");
             expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
